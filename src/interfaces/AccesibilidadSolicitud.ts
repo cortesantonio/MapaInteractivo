614515bb0a1ddf14cc2b3a3af241d50cb80659a8
Marck0777 <ibarramarcelo200@gmail.com>
2025-04-11 16:26:53 -0400

    Eliminacion de archivos

--- src/interfaces/AccesibilidadSolicitud.ts
+++
@@ -1,9 +0,0 @@
-import { Solicitud } from "./Solicitud";
-import { Accesibilidad } from "./Accesibilidad";
-
-export interface AccesibilidadSolicitud {
-    id: number;
-    idSolicitud: Solicitud;        // FK
-    idAccesibilidad: Accesibilidad; // FK
-  }
-  
