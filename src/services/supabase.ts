3e367b98e6c20af28e2aa3a16a4a1762fc873a07
cortesantonio <antonio.cortes.sotelo@gmail.com>
2025-04-21 11:41:39 -0400

    migrado de credenciales a un .env

--- src/services/supabase.ts
+++ src/services/supabase.ts
@@ -1,8 +1,10 @@
 import { createClient } from '@supabase/supabase-js'
 
-const supabaseUrl = 'https://vnttauavodfqkekncxbz.supabase.co'
-const supabaseKey = 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6InZudHRhdWF2b2RmcWtla25jeGJ6Iiwicm9sZSI6ImFub24iLCJpYXQiOjE3NDI5OTM3NTIsImV4cCI6MjA1ODU2OTc1Mn0.mKu5crNJgEkfvj5vDW9aRm3suudqW7oMESnmR0lZUzo'
-export const supabase = createClient(supabaseUrl, supabaseKey)
+const supabaseUrl = import.meta.env.VITE_SUPABASE_URL;
+const supabaseKey = import.meta.env.VITE_SUPABASE_ANON_KEY;
 
+if (!supabaseUrl || !supabaseKey) {
+  throw new Error('Variables de entorno de Supabase no definidas');
+}
 
-// creacion de instancia de llamado a supabase. 
+export const supabase = createClient(supabaseUrl, supabaseKey);
