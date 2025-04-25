cad8b735acdcbb7943b937374442e884da7df79b
DubanPrograDVR <156736859+DubanPrograDVR@users.noreply.github.com>
2025-04-24 21:44:57 -0400

    Se agrego el nombre_locacion,direccion, tipo_recinto y tambien se corrigeron los estilos de inspeccionar reseña, la vista esta en modo escritorio y en modo movil, y se corrigio la forma en que se procesa la imagen ahora es de una etiqueta <img> y no desde los estilos css.

--- src/screens/resenas/Inspeccionar_Resenas.tsx
+++ src/screens/resenas/Inspeccionar_Resenas.tsx
@@ -5,65 +5,50 @@ import { useState, useEffect } from 'react';
 import { supabase } from '../../services/supabase';
 import { Resenas } from '../../interfaces/Resenas';
 
-
 function Inspeccionar_Resenas() {
   const simbolo = ">";
-  let Marcador = 6; 
+  let Marcador_id = 6; 
   const [resenas, setResenas] = useState<Resenas[]>([]);
   useEffect(() => {
     const fetchResenas = async () => {
         const { data, error } = await supabase
           .from('resenas')
-          .select(`
-            id,
-            id_marcador (
-            id
-            ),
-            id_usuario (
-            id,
-            nombre
-            ),
-            fecha,
-            calificacion,
-            comentario
-          `)
-          .eq('id_marcador', Marcador); 
-          
-  
+          .select(`id,id_marcador (id,nombre_recinto,direccion,tipo_recinto (id,tipo)),id_usuario (id,nombre),fecha,calificacion,comentario`).eq('id_marcador', Marcador_id); 
         if (error) {
           console.error("Error al obtener reseñas:", error);
         } else {
           setResenas(data as any);
           console.log("Reseñas obtenidas:", data);
         }
-      
     };
 
     fetchResenas();
   }, []);
 
-  
 
   return (
     <div className="container">
-      <div className="header">
+      <div  className="header">
+        <div className='imagen'>
+          <img src="https://lh3.googleusercontent.com/gps-cs-s/AB5caB9eZeqiYZh_N6HddUd7JMb6o7pqX4RRnEi7nILjYXDI7kkYSnjc_vaeigx7oH_ya-PravH6AY-cDaK_Whg_xln3BIzCQQYzWkoH6xltRO771yV22JQs9BVH0mIQMcRyRveNe0Sd=w426-h240-k-no" alt="" />
+        </div>
         <div className="icono">
           <FontAwesomeIcon icon={faReply} />
         </div>
         <div className="titulo-locacion">
-          <h1>{"Cargando..."}</h1>
+          <h2>{resenas[0]?.id_marcador.nombre_recinto || "Cargando..."}</h2>
         </div>
         <div className="info-locacion">
-          <h4>{simbolo} Anfiteatro</h4>
+          <h4>{simbolo} { (resenas[0]?.id_marcador.tipo_recinto as any)?.tipo || "Cargando tipo..."}</h4>
         </div>
         <div className="text">
-          <span>{ "Dirección no disponible"}</span>
+          <h2>{resenas[0]?.id_marcador.direccion || "Cargando..."}</h2>
         </div>
       </div>
 
       <div className="contenido-reseña">
         <div className="titulo-reseña">
-          <h4>Reseñas</h4>
+          <h4 style={{ paddingLeft: '0' }}>Reseñas</h4>
           <hr />
         </div>
 
@@ -71,8 +56,11 @@ function Inspeccionar_Resenas() {
           <div className="bloque-reseña" key={index}>
             <div className="autor-reseña">
               <h1>{r.id_usuario.nombre}</h1>
-              <div className="trash-button">
-                <FontAwesomeIcon icon={faTrash} />
+              <div style={{ display: 'flex', alignItems: 'center', gap: '8px' }}>
+                <div className="trash-button">
+                  <FontAwesomeIcon icon={faTrash} />
+                </div>
+                
               </div>
             </div>
 
